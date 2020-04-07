db0c6fd292778d6499dadfc9cbe3779ac9f71130
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-04-07 16:27:16 -0300

    Início da implementação do CQRS.

---
+++ ViniciusStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,24 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace ViniciusStore.Domain.StoreContext.Commands.CustomerCommands.Inputs {
+    public class CreateCustomerCommand : Notifiable {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public bool Validate() {
+            AddNotifications(new Contract()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter no mínimo 3 caracteres.")
+                .HasMaxLen(FirstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres.")
+                .HasMinLen(LastName, 3, "LastName", "O nome deve conter no mínimo 3 caracteres.")
+                .HasMaxLen(LastName, 40, "LastName", "O nome deve conter no máximo 40 caracteres.")
+                .IsEmail(Email, "Address", "O e-mail é inválido.")
+                .HasLen(Document, 11, "Document", "CPF inválido.")
+            );
+            return Valid;
+        }
+    }
+}
---
+++ ViniciusStore.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ViniciusStore.Domain.StoreContext.Entities;
+
+namespace ViniciusStore.Domain.StoreContext.Commands.OrderCommands.Inputs {
+    public class PlaceOrderCommand {
+        public Guid Customer { get; set; }
+        public IList<OrderItemCommand> OrderItens { get; set; }
+    }
+
+
+    public class OrderItemCommand {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}

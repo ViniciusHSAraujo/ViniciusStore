6827a83d470e72d31332d409f997868893a4ab5a
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-04-09 16:40:18 -0300

    Início da implementação dos repositórios.

---
+++ ViniciusStore.Domain/StoreContext/Repositories/ICustomeRepository.cs
@@ -0,0 +1,9 @@
+﻿using ViniciusStore.Domain.StoreContext.Entities;
+
+namespace ViniciusStore.Domain.StoreContext.Repositories {
+    public interface ICustomeRepository {
+        bool CheckDocument(string document);
+        bool CheckEmail(string email);
+        void Save(Customer customer);
+    }
+}

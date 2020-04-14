c42582201bc5e680d52990f473a7c3e95b1ad3f6
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-04-14 09:10:37 -0300

    Commit pré troca de máquina.

---
+++ ViniciusStore.API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ViniciusStore.Domain.StoreContext.Entities;
+
+namespace ViniciusStore.API.Controllers {
+    public class HomeController : Controller {
+
+        public List<Customer> Get() {
+            
+        }
+    }
+}
